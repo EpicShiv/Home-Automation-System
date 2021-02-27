2d6dfd9124fe5c89cc2c1c9d102ff83f892e6255
Shiva Teja <shivateja@yahoo.com>
2021-02-28 01:37:21 +0530

    Added feature for selecting type of relay used

--- Home_Automation_System/Home_Automation_System.ino
+++ Home_Automation_System/Home_Automation_System.ino
@@ -17,6 +17,16 @@
   #define DUMPS(s)
 #endif
 
+// If you are using pull up relay then define 'Relay_Type_Pullup 1' else '#define Relay_Type_Pullup 0'
+#define Relay_Type_Pullup 1
+#if Relay_Type_Pullup
+  #define R_High LOW
+  #define R_Low HIGH
+#else
+  #define R_High HIGH
+  #define R_Low LOW
+#endif
+
 int a, b, c, d, e, f, g, h;
 
 // Define the GPIO connected with Relays and switches.
@@ -80,13 +90,13 @@ void relayOnOff(int relay)
     case 1:
     if(!R1_State)
     {
-      digitalWrite(R1, LOW);          // Turns on relay 1.
+      digitalWrite(R1, R_High);          // Turns on relay 1.
       R1_State = 1;                   // Sets or changes the relay 1 state to ON.
       DUMPS("\r\n Relay 1 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R1, HIGH);         // Turns off relay 1.
+      digitalWrite(R1, R_Low);         // Turns off relay 1.
       R1_State = 0;                   // Sets the relay 1 state to OFF.
       DUMPS("\r\n Relay 1 is OFF");   // Useful while debugging.
     }
@@ -95,13 +105,13 @@ void relayOnOff(int relay)
     case 2:
     if(R2_State == 0)
     {
-      digitalWrite(R2, LOW);          // Turns on relay 2.
+      digitalWrite(R2, R_High);          // Turns on relay 2.
       R2_State = 1;                   // Sets or changes the relay 2 state to ON.
       DUMPS("\r\n Relay 2 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R2, HIGH);          // Turns on relay 2.
+      digitalWrite(R2, R_Low);          // Turns on relay 2.
       R2_State = 0;                    // Sets or changes the relay 2 state to OFF.
       DUMPS("\r\n Relay 2 is OFF");    // Useful while debugging.
     }
@@ -110,13 +120,13 @@ void relayOnOff(int relay)
     case 3:
     if(R3_State == 0)
     {
-      digitalWrite(R3, LOW);          // Turns on relay 3.
+      digitalWrite(R3, R_High);          // Turns on relay 3.
       R3_State = 1;                   // Sets or changes the relay 3 state to ON.
       DUMPS("\r\n Relay 3 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R3, HIGH);         // Turns on relay 3.
+      digitalWrite(R3, R_Low);         // Turns on relay 3.
       R3_State = 0;                   // Sets or changes the relay 3 state to OFF.
       DUMPS("\r\n Relay 3 is OFF");   // Useful while debugging.
     }
@@ -125,13 +135,13 @@ void relayOnOff(int relay)
     case 4:
     if(R4_State == 0)
     {
-      digitalWrite(R4, LOW);          // Turns on relay 4.
+      digitalWrite(R4, R_High);          // Turns on relay 4.
       R4_State = 1;                   // Sets or changes the relay 4 state to ON.
       DUMPS("\r\n Relay 4 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R4, HIGH);         // Turns off relay 4.
+      digitalWrite(R4, R_Low);         // Turns off relay 4.
       R4_State = 0;                   // Sets or changes the relay 4 state to OFF.
       DUMPS("\r\n Relay 4 is OFF");   // Useful while debugging.
     }
