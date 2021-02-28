436ad1bf423a78ceec05b77cacd913f4fe64622e
Shiva Teja <shivateja@yahoo.com>
2021-02-28 18:43:48 +0530

    Added feature for selecting type of relay.

--- Home_Automation_System/Home_Automation_System.ino
+++ Home_Automation_System/Home_Automation_System.ino
@@ -90,13 +90,13 @@ void relayOnOff(int relay)
     case 1:
     if(!R1_State)
     {
-      digitalWrite(R1, R_High);          // Turns on relay 1.
+      digitalWrite(R1, R_High);       // Turns on relay 1.
       R1_State = 1;                   // Sets or changes the relay 1 state to ON.
       DUMPS("\r\n Relay 1 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R1, R_Low);         // Turns off relay 1.
+      digitalWrite(R1, R_Low);        // Turns off relay 1.
       R1_State = 0;                   // Sets the relay 1 state to OFF.
       DUMPS("\r\n Relay 1 is OFF");   // Useful while debugging.
     }
@@ -105,13 +105,13 @@ void relayOnOff(int relay)
     case 2:
     if(R2_State == 0)
     {
-      digitalWrite(R2, R_High);          // Turns on relay 2.
+      digitalWrite(R2, R_High);       // Turns on relay 2.
       R2_State = 1;                   // Sets or changes the relay 2 state to ON.
       DUMPS("\r\n Relay 2 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R2, R_Low);          // Turns on relay 2.
+      digitalWrite(R2, R_Low);         // Turns on relay 2.
       R2_State = 0;                    // Sets or changes the relay 2 state to OFF.
       DUMPS("\r\n Relay 2 is OFF");    // Useful while debugging.
     }
@@ -120,13 +120,13 @@ void relayOnOff(int relay)
     case 3:
     if(R3_State == 0)
     {
-      digitalWrite(R3, R_High);          // Turns on relay 3.
+      digitalWrite(R3, R_High);       // Turns on relay 3.
       R3_State = 1;                   // Sets or changes the relay 3 state to ON.
       DUMPS("\r\n Relay 3 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R3, R_Low);         // Turns on relay 3.
+      digitalWrite(R3, R_Low);        // Turns on relay 3.
       R3_State = 0;                   // Sets or changes the relay 3 state to OFF.
       DUMPS("\r\n Relay 3 is OFF");   // Useful while debugging.
     }
@@ -135,13 +135,13 @@ void relayOnOff(int relay)
     case 4:
     if(R4_State == 0)
     {
-      digitalWrite(R4, R_High);          // Turns on relay 4.
+      digitalWrite(R4, R_High);       // Turns on relay 4.
       R4_State = 1;                   // Sets or changes the relay 4 state to ON.
       DUMPS("\r\n Relay 4 is ON");    // Useful while debugging.
     }
     else
     {
-      digitalWrite(R4, R_Low);         // Turns off relay 4.
+      digitalWrite(R4, R_Low);        // Turns off relay 4.
       R4_State = 0;                   // Sets or changes the relay 4 state to OFF.
       DUMPS("\r\n Relay 4 is OFF");   // Useful while debugging.
     }
@@ -154,7 +154,7 @@ void relayOnOff(int relay)
 // This function is used to control relays via manual switches.
 void manualSwitch()
 {
-  // Operation is done by switching to specified relay.
+  // Operation is done by switching to respective relay.
   a = digitalRead(S1);          // Stores the Switch 1 input in variiable 'a'.
   while(!a == b)                // Executes the loop if change in switch 1 state is detected.
   {
